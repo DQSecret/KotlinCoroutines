bbccea791419753d9a11c99024257d608586d448
DQ <dqdanavera@gmail.com>
2019-04-17 19:00:23 +0800

    feat: 帧动画测试

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.coroutines
 
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.frame_anim_deng
+import kotlinx.android.synthetic.main.activity_main.frame_anim_shai_zi
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    // 灯
+    frame_anim_deng.setBackgroundResource(R.drawable.frame_animation_deng)
+    val frameDeng = frame_anim_deng.background as AnimationDrawable
+    frameDeng.start()
+
+    // 骰子
+    frame_anim_shai_zi.setBackgroundResource(R.drawable.frame_animation_shai_zi)
+    val frameShaiZi = frame_anim_shai_zi.background as AnimationDrawable
+    frameShaiZi.start()
   }
 }
