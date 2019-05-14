e82628dcafc4b6afd0c0cc201a2d1520e0bf6b37
DQ <dqdanavera@gmail.com>
2019-05-14 18:45:19 +0800

    feat: 尝试了一下 高斯模糊 - 补充

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -3,23 +3,33 @@ package com.example.coroutines
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.frame_anim_deng
-import kotlinx.android.synthetic.main.activity_main.frame_anim_shai_zi
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    // 灯
-    frame_anim_deng.setBackgroundResource(R.drawable.frame_animation_deng)
-    val frameDeng = frame_anim_deng.background as AnimationDrawable
-    frameDeng.start()
+        // 灯
+        frame_anim_deng.setBackgroundResource(R.drawable.frame_animation_deng)
+        val frameDeng = frame_anim_deng.background as AnimationDrawable
+        frameDeng.start()
 
-    // 骰子
-    frame_anim_shai_zi.setBackgroundResource(R.drawable.frame_animation_shai_zi)
-    val frameShaiZi = frame_anim_shai_zi.background as AnimationDrawable
-    frameShaiZi.start()
-  }
+        // 骰子
+        frame_anim_shai_zi.setBackgroundResource(R.drawable.frame_animation_shai_zi)
+        val frameShaiZi = frame_anim_shai_zi.background as AnimationDrawable
+        frameShaiZi.start()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        blurLayout.startBlur()
+        blurLayout.lockView()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        blurLayout.pauseBlur()
+    }
 }
