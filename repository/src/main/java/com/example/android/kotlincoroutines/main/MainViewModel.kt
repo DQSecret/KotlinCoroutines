bb70ed9b8603b0d381eb5cd53b52c2ac47c6a44c
DQ <dqdanavera@gmail.com>
2019-05-15 11:59:06 +0800

    feat: 使用样板代码

--- repository/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
+++ repository/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
@@ -21,6 +21,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.android.kotlincoroutines.util.singleArgViewModelFactory
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 /**
@@ -113,15 +114,21 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
 //            }
 //        }
 
-        viewModelScope.launch {
-            try {
-                _spinner.value = true
-                repository.refreshTitle()
-            } catch (error: TitleRefreshError) {
-                _snackBar.value = error.message
-            } finally {
-                _spinner.value = false
-            }
+        // 改版后 v1
+//        viewModelScope.launch {
+//            try {
+//                _spinner.value = true
+//                repository.refreshTitle()
+//            } catch (error: TitleRefreshError) {
+//                _snackBar.value = error.message
+//            } finally {
+//                _spinner.value = false
+//            }
+//        }
+
+        // 使用样板代码 v2
+        launchDataLoad {
+            repository.refreshTitle()
         }
     }
 
@@ -137,4 +144,16 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
      *              spinner will stop
      */
     // TODO: Add launchDataLoad here then refactor refreshTitle to use it
+    private fun launchDataLoad(block: suspend () -> Unit): Job {
+        return viewModelScope.launch {
+            try {
+                _spinner.value = true
+                block()
+            } catch (error: TitleRefreshError) {
+                _snackBar.value = error.message
+            } finally {
+                _spinner.value = false
+            }
+        }
+    }
 }
