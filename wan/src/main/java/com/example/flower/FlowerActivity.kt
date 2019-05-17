5a0cd8ce12c782d6a9590d0af479270fdd77d6e7
DQ <dqdanavera@gmail.com>
2019-05-17 14:19:19 +0800

    feat: flower - 初步尝试 Navigation

--- wan/src/main/java/com/example/flower/FlowerActivity.kt
+++ wan/src/main/java/com/example/flower/FlowerActivity.kt
@@ -6,15 +6,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.wan.R
 import com.example.wan.databinding.LayoutFlowerActBinding
+import kotlinx.android.synthetic.main.layout_flower_act.*
 
 /**
  * SunFlower
@@ -31,13 +31,12 @@ class FlowerActivity : AppCompatActivity() {
         }
     }
 
-    private lateinit var binding: LayoutFlowerActBinding
-    private lateinit var drawerLayout: DrawerLayout
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        DataBindingUtil.setContentView<LayoutFlowerActBinding>(this, R.layout.layout_flower_act)
 
         setupContentView()
         setupActionBar()
@@ -45,19 +44,17 @@ class FlowerActivity : AppCompatActivity() {
     }
 
     private fun setupContentView() {
-        binding = DataBindingUtil.setContentView(this, R.layout.layout_flower_act)
-        drawerLayout = binding.drawerLayout
-        navController = Navigation.findNavController(this, R.id.garden_nav_fragment)
-        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        navController = findNavController(R.id.garden_nav_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawer_layout)
     }
 
     private fun setupActionBar() {
-        setSupportActionBar(binding.toolbar)
+        setSupportActionBar(toolbar)
         setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     private fun setupNavigationMenu() {
-        binding.navigationView.setupWithNavController(navController)
+        navigation_view.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -65,8 +62,8 @@ class FlowerActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START)
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
         } else {
             super.onBackPressed()
         }
